ab6c54eb8169647f4583bcc39c00f251dd5bb73b
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2022-04-22 00:30:32 +0200

    chore: update dependencies

--- test/utils/DSTestPlusPlus.sol
+++ test/utils/DSTestPlusPlus.sol
@@ -1,16 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.13;
 
-import { XConsole } from "./Console.sol";
-
 import { DSTestPlus } from "@rari-capital/solmate/test/utils/DSTestPlus.sol";
+import "@std/Test.sol";
 
-import { stdCheats, stdError } from "@std/stdlib.sol";
-import { Vm } from "@std/Vm.sol";
-
-contract DSTestPlusPlus is DSTestPlus, stdCheats {
-    XConsole console = new XConsole();
-
-    /// @dev Use forge-std Vm logic
-    Vm public constant vm = Vm(HEVM_ADDRESS);
-}
+contract DSTestPlusPlus is Test, DSTestPlus {}
