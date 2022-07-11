0ccef661d0b2255cb02a8fe6ca54e3e075aea184
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2022-07-11 12:09:18 +0200

    chore: update remappings

--- test/utils/DSTestPlusPlus.sol
+++ test/utils/DSTestPlusPlus.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.13;
 
-import { DSTestPlus } from "@rari-capital/solmate/test/utils/DSTestPlus.sol";
+import { DSTestPlus } from "@rari-capital/solmate/src/test/utils/DSTestPlus.sol";
 import "@std/Test.sol";
 
 contract DSTestPlusPlus is Test, DSTestPlus {}
